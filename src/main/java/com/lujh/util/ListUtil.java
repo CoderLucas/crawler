e6a16de830beb932f7b0a24fb06544b2f376b747
lujh <lujianhao518@163.com>
2018-04-19 20:09:51 +0800

    0415

--- src/main/java/com/lujh/util/ListUtil.java
+++ src/main/java/com/lujh/util/ListUtil.java
@@ -2,9 +2,7 @@ package com.lujh.util;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by lujianhao on 2018/2/6.
@@ -33,9 +31,57 @@ public class ListUtil {
         return StringUtils.join(list.toArray(), ",");
     }
 
-    public static void main(String[] args) {
+    public static class M implements Comparable<M> {
 
-        String list = null;
-        list.equals("ddd");
+        private int id;
+        private int sort;
+
+        public M(int id, int sort) {
+            this.id = id;
+            this.sort = sort;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            M m = (M) o;
+
+            return id == m.id;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = id;
+            return result;
+        }
+
+        @Override
+        public int compareTo(M o) {
+            if (!this.equals(o)){
+                return Integer.valueOf(o.sort).compareTo(this.sort);
+            }
+            return 0;
+        }
+
+        @Override
+        public String toString() {
+            return "" + id + ":" + sort;
+        }
     }
+
+    public static void main(String[] args) {
+        Set<M> set = new TreeSet<>();
+        set.add(new M(1,1));
+        set.add(new M(2,2));
+        set.add(new M(3,3));
+        set.add(new M(1,3));
+        set.add(new M(1,4));
+
+        for (M m : set) {
+            System.out.println(m);
+        }
+    }
+
 }
